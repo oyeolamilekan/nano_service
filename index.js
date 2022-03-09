a2a1ab321c6cf64e72b64b42e8178ab464e88586
oye johnson <johnsonoye34@gmail.com>
2022-03-09 11:25:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import WAValidator from "@swyftx/api-crypto-address-validator"
+
+const app = express();
+const port = 3000;
+
+app.get("/validate_address/:currency/:address", (req, res) => {
+    try {
+        const {
+            currency,
+            address
+        } = req.params;
+        const valid = WAValidator.validate(address, currency);
+        console.log(fff);
+        res.status(200).send({
+            "is_valid": valid
+        }).end()
+    } catch (error) {
+        res.status(500).send({
+            "msg": "something bad happened"
+        }).end()
+    }
+});
+
+app.listen(port, () => console.log("listening on port"));
